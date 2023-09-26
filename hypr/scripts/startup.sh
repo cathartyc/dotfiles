15c665f6a809b3e2ab24230a20af9efdd7c29c0c
cathartyc <67878876+cathartyc@users.noreply.github.com>
2023-09-26 15:47:34 +0200

    clean up and add motivation for the funny line

--- hypr/scripts/startup.sh
+++ hypr/scripts/startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# copyq --start-server &
+copyq --start-server
 swaybg -i ~/.config/hypr/pcb_wallpaper.jpg -m fill &
 waybar &
-# nextcloud &
 swayidle -w &
+# Workaround: for some reasons, xdg-desktop-portal-hyprland goes crazy at startup 
 systemctl --user restart xdg-desktop-portal-hyprland.service
-#-m fill -i /home/cathartyc/Pictures/wires_wallpaper.jpg # & firefox
 
