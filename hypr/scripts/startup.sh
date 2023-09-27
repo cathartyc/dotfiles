178e9532a3d2218f4d680e6b1f71bc7a81c95a79
cathartyc <67878876+cathartyc@users.noreply.github.com>
2023-09-27 16:36:01 +0200

    brightness control shortcuts now are mirrored to the external monitor

--- hypr/scripts/startup.sh
+++ hypr/scripts/startup.sh
@@ -5,4 +5,6 @@ waybar &
 swayidle -w &
 # Workaround: for some reasons, xdg-desktop-portal-hyprland goes crazy at startup 
 systemctl --user restart xdg-desktop-portal-hyprland.service
+ddcutil setvcp 10 40
+brightnessctl set 40%
 
