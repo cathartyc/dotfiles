c477a01b26275544a39b2dfde17d62525b17d655
cathartyc <67878876+cathartyc@users.noreply.github.com>
2024-11-05 17:12:13 +0100

    whole setup dump

--- hypr/scripts/startup.sh
+++ hypr/scripts/startup.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-copyq --start-server
-swaybg -i ~/.config/hypr/wallpapers/balls.jpg -m fill &
+sway-audio-idle-inhibit &
+#dbus-update-activation-environment --systemd --all
+#systemctl --user import-environment QT_QPA_PLATFORMTHEME
+wl-paste --watch cliphist store &
+#wl-paste --type image --watch cliphist store &
+swaybg -i ~/.config/hypr/wallpapers/anime_girls_cap.png -m fill &
 waybar &
+nextcloud --background &
 swayidle -w &
+otd-daemon &
 ddcutil setvcp 10 40
 brightnessctl set 40%
-
+/usr/libexec/polkit-gnome-authentication-agent-1 &
+swayosd-server &
+AppImageLauncher "$HOME"/Applications/Bitwarden*.AppImage &
+$XDG_CONFIG_HOME/hypr/scripts/auto-vpn.sh >$HOME/vpn.log &
