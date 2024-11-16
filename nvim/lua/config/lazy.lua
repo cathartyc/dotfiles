2c4f049f2371cd3055c293bd184c29a86b7a8c6c
cathartyc <67878876+cathartyc@users.noreply.github.com>
2024-11-16 16:12:33 +0100

    [nvim] extract which-key config

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -36,19 +36,6 @@ vim.o.listchars = 'tab:» ,lead:•,trail:•'
 require("lazy").setup({
     { import = "plugins" },
     checker = { enabled = true },
-    {
-        "folke/which-key.nvim",
-        event = "VeryLazy",
-        init = function()
-            vim.o.timeout = true
-            vim.o.timeoutlen = 300
-        end,
-        opts = {
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
-    },
     { "folke/neoconf.nvim", cmd = "Neoconf" },
     {
         "folke/lazydev.nvim",
