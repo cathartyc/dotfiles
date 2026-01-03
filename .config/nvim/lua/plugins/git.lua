38e3510daf76a2719cc8ff5190cdc3e5e542842c
cathartyc <67878876+cathartyc@users.noreply.github.com>
2026-01-03 12:47:35 +0100

    [nvim] add git keybindings

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,3 +1,15 @@
 return {
-  "lewis6991/gitsigns.nvim"
+  "lewis6991/gitsigns.nvim",
+  keys = {
+      { "<leader>g",  group = "Git", desc = "git (gitsigns)" },
+      { "<leader>gb", function() require("gitsigns").blame_line() end, desc = "blame line" },
+      { "<leader>gs",  group = "stage", desc = "stage" },
+      { "<leader>gsh", function() require("gitsigns").stage_hunk() end, desc = "stage hunk" },
+      { "<leader>gsf", function() require("gitsigns").stage_buffer() end, desc = "stage buffer" },
+      { "<leader>gr",  group = "reset", desc = "reset" },
+      { "<leader>grh", function() require("gitsigns").reset_hunk() end, desc = "reset hunk" },
+      { "<leader>grb", function() require("gitsigns").reset_buffer() end, desc = "reset buffer" },
+
+
+  }
 }
